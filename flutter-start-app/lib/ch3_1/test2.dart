92a877c0777058566b241a0eceb6e040703dba61
isaac <isaac7263@naver.com>
2023-10-10 23:52:46 +0900

    a: 외부 패키지 사용 예제 및 as, show, hide 키워드 사용 예제

---
+++ flutter-start-app/lib/ch3_1/test2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_start/ch3_1/test1.dart';
+
+main() {
+  no1 = 10;
+  // _no2 = 20; // error
+
+  sayHello1();
+  // _sayHello2(); //error
+
+  User1 user1 = User1();
+  user1.name = "isaac";
+  // user1._address = "seoul"; // error
+
+  // _User2 user2 = _User2(); // error
+}
+
---
+++ flutter-start-app/lib/ch3_1_2_as_show_hide/test1.dart
@@ -0,0 +1,8 @@
+import '../ch3_1/test1.dart' as A;
+
+main() {
+  // no1 = 20; //error
+  A.no1 = 20;
+  A.sayHello1();
+  A.User1 user;
+}
