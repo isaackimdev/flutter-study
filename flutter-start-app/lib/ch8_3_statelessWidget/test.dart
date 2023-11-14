f1b804247f4e2cc2cf12566f13579551e4fbfa1f
isaac <isaac7263@naver.com>
2023-11-14 20:38:11 +0900

    m: 8-3 stateless widget

--- flutter-start-app/lib/ch8_3_statelessWidget/test.dart
+++ flutter-start-app/lib/ch8_3_statelessWidget/test.dart
@@ -5,6 +5,18 @@ main() {
 }
 
 class MyApp extends StatelessWidget {
+  bool enabled = false;
+  String stateText = "disabled";
+
+  void changeCheck() {
+    if (enabled) {
+      enabled = false;
+      stateText = "disabled";
+    } else {
+      enabled = true;
+      stateText = "enable";
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,8 +26,17 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('test')
+              IconButton(
+                  onPressed: changeCheck,
+                  icon: (enabled ? Icon(Icons.check_box, size: 20,) : Icon(Icons.check_box_outline_blank, size: 20,)),
+                color: Colors.red,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 16),
+                child: Text('$stateText', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+              )
             ],
           ),
         ),
