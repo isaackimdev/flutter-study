1907edf313e45e5fed16a04cc4d327794550b1a7
isaac <isaac7263@naver.com>
2023-11-14 11:42:21 +0900

    a: 8-3 stateless widget

---
+++ flutter-start-app/lib/ch8_3_statelessWidget/test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Statefull Test'),
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Text('test')
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+}
