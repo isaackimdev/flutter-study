be6973d07ce3ae7c639f541564a78a03aef2780b
isaac <isaac7263@naver.com>
2023-11-29 18:15:56 +0900

    a: 13-5. sliverAppBar a: 14-1. navigator + route

---
+++ flutter-start-app/lib/ch14_1_navigation/one_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class OneScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('OneScreen'),
+        ),
+        body: Container(
+          color: Colors.red,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('OneScreen', style: TextStyle(color: Colors.white, fontSize: 30),),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/two');
+                  },
+                  child: Text('Go Two'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
